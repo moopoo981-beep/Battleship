234a72cad26460b7c0d0f9b4217641efe51438e9
moopoo981-beep <moopoo981@gmail.com>
2026-09-10 17:43:07 +0700

    Add files via upload

---
+++ dist/config.js
@@ -0,0 +1,3 @@
+// Set the HTTPS URL of your deployed room server. Leave blank for solo play.
+// A server that also serves this game will be detected automatically.
+window.IRON_TIDE_CONFIG = { serverUrl: "" };
